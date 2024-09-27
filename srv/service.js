bd82c32b73ef81ce477b81b167f1095b6b8f77af
Carlos Furigo <cfurigo@coachit.com.br>
2024-09-27 18:43:17 +0000

    Ajustes

--- srv/service.js
+++ srv/service.js
@@ -3,8 +3,11 @@ const cds = require('@sap/cds');
 * Implementacao do servico definido em /service.cds
 */
 module.exports = cds.service.impl(async function() {
+	
 	const bupa = await cds.connect.to('API_BUSINESS_PARTNER');
+	
 	const {Customer, Supplier} = this.entities;
+
 	this.on('READ', [Customer, Supplier], async req => {
 		return bupa.run(req.query);
 	});
